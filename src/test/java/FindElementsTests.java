bb47e3de2f578bdbdaafbfcad4b5b71d2d5f816f
Sergey Shvets <serega.shvets21@gmail.com>
2024-07-18 20:14:54 +0200

    Method FindTagName

--- src/test/java/FindElementsTests.java
+++ src/test/java/FindElementsTests.java
@@ -1,10 +1,13 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.List;
 
 public class FindElementsTests {
     WebDriver driver;
@@ -13,15 +16,27 @@ public class FindElementsTests {
     public void setUp() {
         driver = new ChromeDriver();
         driver.get("https://www.ilcarro.web.app");
-
-        driver.manage().window().maximize();
+        // driver.manage().window().setPosition(new Point(2500, 0)); // Подвинуть окно браузера на 2500 пикселей вправо, чтобы он запускался на втором мониторе
+        driver.manage().window().maximize(); // На весь экран развернуть браузер
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(1000)); // Ожидание локатора
 
     }
 
+    // Ищем локатор элемента по тэгу в DOM-дереве сайта
     @Test
-    public void testFindElementsTadName(){
+    public void testFindElementsTagName() {
+        // Локатор с тегом <h1>
+        WebElement h1 = driver.findElement(By.tagName("h1"));
+        System.out.println(h1.getText());
 
+        // Локатор с тегом <а> это картинка в этом случае
+        WebElement a = driver.findElement(By.tagName("a"));
+        System.out.println(a.getSize()); // Узнаём разрешение картинки в состоянии рендера
+
+        // Массив локаторов с тегом <а>
+        List<WebElement> elements_a = driver.findElements(By.tagName("a"));
+        // System.out.println(elements_a); Распечатает нам все элементы массива - что лишнее
+        System.out.println(elements_a.size()); // Узнаём размер массива 31 элемента на странице с тегом <а>
     }
 
     @AfterMethod(enabled = true) //включение или отключение закрытия браузера посте тестов
